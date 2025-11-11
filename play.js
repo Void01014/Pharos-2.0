f5b6944f0e1f77c516ce1b91ee830b4ccedf36b9
Omar EL FILALI <omarelfiie2007@gmail.com>
2025-11-11 12:02:52 +0100

    added drag feature on cards, plus some fix and small styling

--- play.js
+++ play.js
@@ -14,13 +14,48 @@ window.addEventListener("DOMContentLoaded", () => {
         navbar.classList.remove('open');
     });
 
+//Opponent deck
+
+let deck_opp = [
+    { id: 0, rarity: 'com', image: 'img/images.ygoprodeck.com_images_cards_102380.jpg', name: '102380', price: 2, heartSrc: 'img/heart(empty).png', attack: 400, defense: 200 },
+    { id: 0, rarity: 'com', image: 'img/images.ygoprodeck.com_images_cards_102380.jpg', name: '102380', price: 2, heartSrc: 'img/heart(empty).png', attack: 400, defense: 200 },
+    { id: 0, rarity: 'com', image: 'img/images.ygoprodeck.com_images_cards_102380.jpg', name: '102380', price: 2, heartSrc: 'img/heart(empty).png', attack: 400, defense: 200 },
+    { id: 0, rarity: 'com', image: 'img/images.ygoprodeck.com_images_cards_102380.jpg', name: '102380', price: 2, heartSrc: 'img/heart(empty).png', attack: 400, defense: 200 },
+    { id: 6, rarity: 'rare', image: 'img/images.ygoprodeck.com_images_cards_176393.jpg', name: '176393', price: 4, heartSrc: 'img/heart(empty).png', attack: 600, defense: 600},
+    { id: 6, rarity: 'rare', image: 'img/images.ygoprodeck.com_images_cards_176393.jpg', name: '176393', price: 4, heartSrc: 'img/heart(empty).png', attack: 600, defense: 600},
+    { id: 6, rarity: 'rare', image: 'img/images.ygoprodeck.com_images_cards_176393.jpg', name: '176393', price: 4, heartSrc: 'img/heart(empty).png', attack: 600, defense: 600},
+    { id: 1, rarity: 'sup_rare', image: 'img/images.ygoprodeck.com_images_cards_109401.jpg', name: '109401', price: 5, heartSrc: 'img/heart(empty).png', attack: 1200, defense: 1000 },
+    { id: 1, rarity: 'sup_rare', image: 'img/images.ygoprodeck.com_images_cards_109401.jpg', name: '109401', price: 5, heartSrc: 'img/heart(empty).png', attack: 1200, defense: 1000 },
+    { id: 3, rarity: 'sup_rare', image: 'img/tarry_Knight_Orbitael.jpg', name: 'Limit_reverse', price: 1, heartSrc: 'img/heart(empty).png', attack: 400, defense: 2000 },
+    { id: 3, rarity: 'sup_rare', image: 'img/tarry_Knight_Orbitael.jpg', name: 'Limit_reverse', price: 1, heartSrc: 'img/heart(empty).png', attack: 400, defense: 2000 },
+    { id: 2, rarity: 'ult_rare', image: 'img/Magnum_The_Reliever.jpg', name: '111280', price: 2, heartSrc: 'img/heart(empty).png', attack: 1800, defense: 1000 },
+    { id: 2, rarity: 'ult_rare', image: 'img/Magnum_The_Reliever.jpg', name: '111280', price: 2, heartSrc: 'img/heart(empty).png', attack: 1800, defense: 1000 },
+    { id: 7, rarity: 'ult_rare', image: 'img/Jet_Warrior.jpg', name: 'Jet_Warrior', price: 7, heartSrc: 'img/heart(empty).png', attack: 2000, defense: 1800 },
+    { id: 7, rarity: 'ult_rare', image: 'img/Jet_Warrior.jpg', name: 'Jet_Warrior', price: 7, heartSrc: 'img/heart(empty).png', attack: 2000, defense: 1800 },
+    { id: 13, rarity: 'sec_rare', image: 'img/Burial_Bicorn.jpg', name: 'Burial_Bicorn', price: 5, heartSrc: 'img/heart(empty).png', attack: 2800, defense: 2500},
+    { id: 13, rarity: 'sec_rare', image: 'img/Burial_Bicorn.jpg', name: 'Burial_Bicorn', price: 5, heartSrc: 'img/heart(empty).png', attack: 2800, defense: 2500},
+    { id: 15, rarity: 'sec_rare', image: 'img/Heretical_Phobos_Covos.jpg', name: 'Heretical_Phobos_Covos', price: 5, heartSrc: 'img/heart(empty).png', attack: 1800, defense: 3000 },
+    { id: 15, rarity: 'sec_rare', image: 'img/Heretical_Phobos_Covos.jpg', name: 'Heretical_Phobos_Covos', price: 5, heartSrc: 'img/heart(empty).png', attack: 1800, defense: 3000 },
+    { id: 5, rarity: 'sec_rare', image: 'img/Ten_Thousand_Dragon.jpg', name: 'Ten_Thousand_Dragon', price: 8, heartSrc: 'img/heart(empty).png', attack: 2900, defense: 1000 }
+];
+
+// Creating the player's deck
+let deck_cards = []
+
+cardData.forEach(card => {
+    deck_cards.push(card.id)
+})
+
+const deck = document.getElementById('card-container');
+const hand_e = document.getElementById('hand');
+const op_hand_e = document.getElementById('op_hand');
 
 //This is the function that creates cards:
 function createCardHTML(card) {
     return `
-        <div class="absolute bottom-0 card com" id="${card.id}">
-            <div class="relative">
-                <img class="w-[calc(2rem+7vw)] md:w-[calc(2rem+12vw)] rounded-[10px]" src="${card.image}" alt="">
+        <div class="absolute bottom-0 card com" id="${card.id} draggable="true">
+            <div class="relative" draggable="true">
+                <img class="w-[calc(2rem+7vw)] md:w-[calc(2rem+12vw)] rounded-[10px]" src="${card.image}" alt="" draggable="false">
                 <div class="absolute bottom-0 w-full h-[27%] z-10">
                     <div class="flex justify-center items-center absolute bottom-0 left-0 h-full w-[50%] bg-[#C300FF] rounded-[10px] shadow-[0_0px_20px_#C210BF]"><p class="attack text-xl md:text-3xl select-none">${card.attack}</p></div>
                     <div class="flex justify-center items-center absolute bottom-0 right-0 h-full w-[50%] bg-cyan-500 rounded-[10px] shadow-[0_0px_20px_cyan]"><p class="defense text-xl md:text-3xl select-none">${card.defense}</p></div>
@@ -29,31 +64,63 @@ function createCardHTML(card) {
         </div>
     `;
 }
+let fact1 = 0;
 function createCardHTML_opp(card) {
+    fact1 = fact1 + 1;
     return `
-        <div class="card com rotate-180" id="${card.id}">
+        <div class="w-[calc(2rem+2.5vw)] md:w-[calc(2rem+6vw)] card com rotate-180 top-[calc(15px*${fact1/(op_hand_e.children.length + 2)})]" id="${card.id}" draggable="false">
             <div class="relative">
-                <img class="w-[calc(2rem+7vw)] md:w-[calc(2rem+12vw)] rounded-[10px]" src="/img/back_cover.png" alt="">
+                <img class="rounded-[10px]" src="/img/back_cover.png" alt="">
             </div>
         </div>
     `;
 }
 
-const container = document.getElementById('card-container');
+
+let fact2 = 0; 
+function createCardHTML_deck(card) {
+    fact2 = fact2 + 1;
+    return `
+        <div class="absolute w-[calc(2rem+4.5vw)] md:w-[calc(2rem+10vw)]  bottom-0 card com bottom-[calc(15px*${fact2/(deck.children.length + 2)})] left-8" id="${card.id}" draggable="false">
+            <div class="relative">
+                <img class="rounded-[10px]" src="/img/back_cover.png" alt="" draggable="false">
+            </div>
+        </div>
+    `;
+}
+
 const allCardsHTML = cardData.map(card => createCardHTML(card)).join('');
+const allCardsHTML_deck = cardData.map(card => createCardHTML_deck(card)).join('');
+const allCardsHTML_opp = deck_opp.map(card => createCardHTML_opp(card)).join('');
 
-container.innerHTML = allCardsHTML;
+deck.innerHTML = allCardsHTML_deck;
 
-const hand = document.getElementById('hand');
-const op_hand = document.getElementById('op_hand');
+
 const firstCard = createCardHTML(cardData[0]);
 const firstCard_opp = createCardHTML_opp(cardData[0]);
 
-hand.innerHTML = firstCard;
-op_hand.innerHTML = firstCard_opp;
+console.log(deck)
+hand.innerHTML = allCardsHTML;
+op_hand.innerHTML = allCardsHTML_opp;
 
 
+hand.addEventListener('mouseover', function () {
+    Array.from(hand.children).forEach(child => {
+        child.classList.add('relative');
+    })
+    hand.style.height = '80vh';
+    hand.style.overflowY = 'scroll';
+    hand.style.overflowX = 'visible';
+});
+hand.addEventListener('mouseout', function () {
+    Array.from(hand.children).forEach(child => {
+        child.classList.remove('relative');
+    })
+    hand.style.height = '26vh';
+    hand.style.overflowY = 'visible';
+});
 
+ 
     
 
 });
